c6c45681adc48a2f126af8ad06977374955b72be
maybeenang <muhammad.120140194@student.itera.ac.id>
2024-10-16 03:11:32 +0700

    conf blade

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,38 +1,54 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-      "m-demare/hlargs.nvim",
-    },
-    -- lazy = false,
-    lazy = true,  -- Lazy-load
-    event = { "BufRead", "BufNewFile" },
-    build = ":TSUpdate",
-    config = function()
-      -- WINDOWS 10
-      require("nvim-treesitter.install").compilers = { "zig" }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"m-demare/hlargs.nvim",
+		},
+		-- lazy = false,
+		lazy = true, -- Lazy-load
+		event = { "BufRead", "BufNewFile" },
+		build = ":TSUpdate",
+		config = function()
+			-- WINDOWS 10
+			require("nvim-treesitter.install").compilers = { "zig" }
 
-      require("nvim-treesitter.configs").setup {
-        highlight = {
-          enable = true,
-        },
-        indent = {
-          enable = true,
-        },
-        ensure_installed = {
-          "lua",
-          -- "javascript",
-          -- "typescript",
-          -- "tsx",
-          -- "json",
-          -- "html",
-          -- "css",
-          -- "php",
-        },
-        sync_install = false,
-        auto_install = true,
-      }
-    end,
-  },
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+			parser_config.blade = {
+				install_info = {
+					url = "https://github.com/EmranMR/tree-sitter-blade",
+					files = {
+						"src/parser.c",
+						-- 'src/scanner.cc',
+					},
+					-- generate_requires_npm = true,
+					requires_generate_from_grammar = true,
+					branch = "main",
+				},
+				filetype = "blade",
+			}
+
+			require("nvim-treesitter.configs").setup({
+				highlight = {
+					enable = true,
+				},
+				indent = {
+					enable = true,
+				},
+				ensure_installed = {
+					"lua",
+					-- "javascript",
+					-- "typescript",
+					-- "tsx",
+					-- "json",
+					-- "html",
+					-- "css",
+					-- "php",
+				},
+				sync_install = false,
+				auto_install = true,
+			})
+		end,
+	},
 }
